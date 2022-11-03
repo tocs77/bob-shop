85334a07667af153d73043f056d4550053edc750
Ilya Golubkov <ilya.golubkov@gmail.com>
2022-11-03 18:58:23 +0600

    Added product router

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import { products } from './data/products.js';
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -9,15 +12,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3031;
 
-app.get('/api', (req, res) => {
-  res.send('API is working');
-});
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Bob-shop server started in ${process.env.NODE_ENV} mode on port ${PORT}`));
